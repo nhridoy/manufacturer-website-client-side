52fd10db140d78cb065bf237e2a37e98feaafd6d
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-05-21 23:53:42 +0600

    Footer Component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <div>
+                <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+            </div>
+        </footer>
     )
 }
 
