7ed236b5335e4030d3e962fefef0777865952fc0
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-05-23 00:24:54 +0600

    New Blog Form Page

--- src/pages/NewBlog/NewBlog.js
+++ src/pages/NewBlog/NewBlog.js
@@ -1,7 +1,51 @@
 import React from "react";
 
 const NewBlog = () => {
-  return <div>NewBlog</div>;
+  return (
+    <div className="container mx-auto px-4">
+      <form action="">
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="label-text">Title?</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+          <label className="label">
+            <span className="label-text-alt">Alt label</span>
+          </label>
+        </div>
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="label-text">Image?</span>
+          </label>
+          <input type="file" className="input input-bordered w-full" />
+          <label className="label">
+            <span className="label-text-alt">Alt label</span>
+          </label>
+        </div>
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="label-text">Title?</span>
+          </label>
+          <textarea
+            class="textarea textarea-bordered h-24"
+            placeholder="Bio"
+          ></textarea>
+          <label className="label">
+            <span className="label-text-alt">Alt label</span>
+          </label>
+        </div>
+        <div className="form-control w-full">
+          <button className="btn btn-primary">
+            <span className="btn-text">Submit</span>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default NewBlog;
