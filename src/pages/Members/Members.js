c6e225144ad2a4eaa92ff692eaa7c2604269ba0f
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-05-23 00:13:26 +0600

    Members Page

--- src/pages/Members/Members.js
+++ src/pages/Members/Members.js
@@ -1,6 +1,42 @@
 import React from "react";
 
 const Members = () => {
-  return <div>Members</div>;
+  return (
+    <div>
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl font-semibold">Members</h1>
+      </div>
+      <div className="table-responsive">
+        <table className="table table-bordered w-full text-center">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>email</td>
+              <td>Admin</td>
+              <td>
+                <button className="btn btn-primary">Make Admin</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Name</td>
+              <td>email</td>
+              <td>Admin</td>
+              <td>
+                <button className="btn btn-primary">Make Admin</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 export default Members;
