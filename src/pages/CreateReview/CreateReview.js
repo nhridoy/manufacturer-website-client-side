22b4e8e8481d35964eaff17bc0ebb01aab408b2f
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-05-23 01:18:35 +0600

    Rating Page Design Done

--- src/pages/CreateReview/CreateReview.js
+++ src/pages/CreateReview/CreateReview.js
@@ -1,6 +1,30 @@
 import React from "react";
 
 const CreateReview = () => {
-  return <div>CreateReview</div>;
+  return (
+    <div className="container mx-auto px-4">
+      <form action="">
+        <div className="form-control">
+          <label htmlFor="">Title</label>
+          <input type="text" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="">Description</label>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            className="input input-bordered"
+          ></textarea>
+        </div>
+        <div className="form-control">
+          <label htmlFor="">Rating</label>
+          <input type="number" className="input input-bordered" />
+        </div>
+        <button className="btn btn-primary mt-4">Add Review</button>
+      </form>
+    </div>
+  );
 };
 export default CreateReview;
