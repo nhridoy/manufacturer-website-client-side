52f4f82b1cebca30e1c6cdedf1b27314c03e45bd
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-05-26 00:37:54 +0600

    notfound

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-            <div>
-                <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <div>
+        <p>Copyright © 2022 - All right reserved by ToolKit Industries Ltd</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
