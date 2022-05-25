977398b604b59f66599d8ed12ef40b2e5ffbaecf
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-05-25 21:51:35 +0600

    Admin Create funtionality

--- src/pages/Members/Members.js
+++ src/pages/Members/Members.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { useQuery } from "react-query";
 import interceptors from "../../utils/interceptors";
 
 const Members = () => {
-  const [members, setMembers] = React.useState([]);
-  React.useEffect(() => {
-    interceptors.get("/users").then((res) => {
-      setMembers(res.data);
-    });
-  }, []);
+  const {
+    data: members,
+    isLoading,
+    refetch,
+  } = useQuery("members", () => {
+    return interceptors.get("/users");
+  });
+
+  const makeAdmin = (email) => {
+    interceptors
+      .put(`/user/admin`, {
+        email,
+      })
+      .then((res) => {
+        refetch();
+      });
+  };
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
   return (
     <div>
       <div className="flex justify-between items-center mb-4">
@@ -24,20 +41,24 @@ const Members = () => {
             </tr>
           </thead>
           <tbody>
-            {members.map((member) => (
+            {members?.data?.map((member) => (
               <tr key={member._id}>
                 <td>{member?.name || "John Doe"}</td>
                 <td>{member?.email}</td>
                 <td>{member?.role || "Member"}</td>
                 <td>
-                  <button
-                    className="btn btn-primary"
-                    onClick={() => {
-                      console.log(member);
-                    }}
-                  >
-                    Make Admin
-                  </button>
+                  {member?.role !== "admin" ? (
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => makeAdmin(member?.email)}
+                    >
+                      Make Admin
+                    </button>
+                  ) : (
+                    <button className="btn btn-primary " disabled>
+                      Admin
+                    </button>
+                  )}
                 </td>
               </tr>
             ))}
