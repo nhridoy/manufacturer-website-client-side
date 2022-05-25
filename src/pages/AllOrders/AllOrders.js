0aeefb6920445f93a4027383e17d0d985cee1cae
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-05-26 00:23:16 +0600

    Status change

--- src/pages/AllOrders/AllOrders.js
+++ src/pages/AllOrders/AllOrders.js
@@ -10,6 +10,14 @@ const AllOrders = () => {
   if (isLoading) {
     return <div>Loading...</div>;
   }
+  const handleShip = (id) => {
+    interceptors
+      .patch(`updateOrders/${id}`, {
+        shipped: true,
+      })
+      .then(() => refetch());
+  };
+
   return (
     <div>
       <div className="flex justify-between items-center mb-4">
@@ -35,7 +43,25 @@ const AllOrders = () => {
                 <td>{order?._id}</td>
                 <td>{order?.name}</td>
                 <td>{order?.quantity}</td>
-                <td>{order?.paid || "UnPaid"}</td>
+                <td>
+                  {order?.paid ? (
+                    order?.status === "shipped" ? (
+                      order?.status
+                    ) : (
+                      <>
+                        <p>{order?.status}</p>
+                        <button
+                          onClick={() => handleShip(order?._id)}
+                          className="btn btn-success btn-sm"
+                        >
+                          Ship
+                        </button>
+                      </>
+                    )
+                  ) : (
+                    "UnPaid"
+                  )}
+                </td>
               </tr>
             ))}
           </tbody>
