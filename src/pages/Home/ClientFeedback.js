ca2ef4639a79bcf2d1291dea18a5e2099ca57f75
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-05-26 03:13:01 +0600

    Show Ratings

--- src/pages/Home/ClientFeedback.js
+++ src/pages/Home/ClientFeedback.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import interceptors from "../../utils/interceptors";
 
 const ClientFeedback = () => {
+  const [feedbacks, setFeedbacks] = React.useState([]);
+  useEffect(() => {
+    interceptors.get("/reviews").then((res) => {
+      setFeedbacks(res.data);
+    });
+  }, []);
   return (
     <section className="text-gray-600 body-font ">
       <div className="container lg:px-5 py-24 mx-auto">
@@ -12,69 +19,48 @@ const ClientFeedback = () => {
         {/* ClientFeedback */}
 
         <div className="grid lg:grid-cols-3  gap-5   ">
-          {[1, 2, 3, 4, 5, 6, 7, 8].map((feedback, index) => (
-            <div key={index} className="lg:col-span-1 w-5/6 mx-auto redShadow">
+          {feedbacks.map((feedback, index) => (
+            <div
+              key={feedback?._id}
+              className="lg:col-span-1 w-5/6 mx-auto redShadow"
+            >
               <div
                 className="bg-white w-full border-2 border-red-600 rounded-lg px-6 py-16 mb-6 shadow sm:inline-block transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:drop-shadow-lg duration-300
 "
               >
                 <div className=" text-center">
-                  <div className="flex-shrink-0">
-                    <div className="inline-block relative">
-                      <a href="#" className="block relative">
-                        <div className="mx-auto object-cover rounded-full h-16 w-16 ">
-                          <img
-                            src="https://api.lorem.space/image/face?hash=33791"
-                            layout=""
-                            className=""
-                            alt="profil"
-                          />
-                        </div>
-                      </a>
-                    </div>
-                  </div>
                   <div className="">
                     <p className="flex justify-center items-center">
-                      <span className="text-gray-900">Charlie Rabiller</span>
+                      <span className="text-gray-900">
+                        {feedback?.user?.name}
+                      </span>
+                    </p>
+                    <p className="flex justify-center items-center">
+                      <span className="text-gray-900">
+                        {feedback?.user?.email}
+                      </span>
                     </p>
                     <div className="flex items-center justify-center mt-1">
                       <div className="rating">
-                        <input
-                          type="radio"
-                          name="rating-2"
-                          className="mask mask-star-2 bg-orange-400"
-                        />
-                        <input
-                          type="radio"
-                          name="rating-2"
-                          className="mask mask-star-2 bg-orange-400"
-                        />
-                        <input
-                          type="radio"
-                          name="rating-2"
-                          className="mask mask-star-2 bg-orange-400"
-                        />
-                        <input
-                          type="radio"
-                          name="rating-2"
-                          className="mask mask-star-2 bg-orange-400"
-                        />
-                        <input
-                          type="radio"
-                          name="rating-2"
-                          className="mask mask-star-2 bg-orange-400"
-                        />
+                        {[...Array(parseInt(feedback?.rating))].map((_, i) => (
+                          <input
+                            key={i}
+                            name="rating-2"
+                            className="mask mask-star-2 bg-orange-400"
+                          />
+                        ))}
                       </div>
                     </div>
                   </div>
                 </div>
+                <h2 className=" text-center font-bold">
+                  <span className="text-gray-900">{feedback?.title}</span>
+                </h2>
                 <div className="mt-3">
                   <div className=" h-6 text-4xl  text-left text-black">“</div>
 
                   <p className="max-w-xs font-light text-gray-900 text-center">
-                    My first job of scanning photos at the Memories 2 Digital
-                    Photo Scanning was fantastic. She completed the work quickly
-                    while I was waiting. Thanks for a great service..
+                    {feedback?.description}
                   </p>
                   <div className="h-7 text-4xl text-right text-black ">“</div>
                 </div>
